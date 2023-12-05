ee19f9d20beb8dfed4e88c7e387daf3201261138
asfaslkdjalks <132086068+asfaslkdjalks@users.noreply.github.com>
2023-12-04 23:10:08 -0500

    Update celery_config.py

--- celery_config.py
+++ celery_config.py
@@ -1,28 +1,29 @@
-from celery import Celery
-import logging
+# import necessary libraries
+from celery import Celery  # import the celery class for task management
+import logging  # import the logging module for logging configuration
 
-# Set up logging
+# set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)  # create a logger instance with the current module's name
 
+
+# create a celery application instance
 celery_app = Celery(
-    __name__,
-    broker='amqp://guest:guest@rabbitmq:5672//',
-    backend='rpc://',
-    include=['tasks']
+    __name__,  
+    broker='amqp://guest:guest@rabbitmq:5672//',  # rabbitmq message broker configuration
+    backend='rpc://',  # result backend configuration using rpc
+    include=['tasks']  # list of modules to include for tasks
 )
 
-# You can also add other Celery configurations here
+# you can also add other celery configurations here
 celery_app.conf.update(
-    task_serializer='json',
-    accept_content=['json'],  # Ignore other content
-    result_serializer='json',
-    timezone='Europe/London',
+    task_serializer='json',  
+    accept_content=['json'],  
+    result_serializer='json',  
+    timezone='europe/london',  
     enable_utc=True,
 )
 
-# Ensure the logger is configured for use in Celery tasks
-celery_app.log.setup_task_loggers()
+celery_app.log.setup_task_loggers() # ensure the logger is configured for use in celery tasks
 
-# Export the logger so it can be imported in other modules
-__all__ = ['celery_app', 'logger']
+__all__ = ['celery_app', 'logger']  # export 'celery_app' and 'logger' for use in other modules
